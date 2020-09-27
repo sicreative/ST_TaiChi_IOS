a7219d1e939e9d1042d3146ddcb1d238f568ffef
sicreative <SIcreativesoftware@gmail.com>
2020-09-27 21:34:50 +0800

    Initial Commit

---
+++ taichi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  taichi
+//
+//  Created by slee on 27/9/2020.
+//  Copyright © 2020 sclee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
